459a5cc2d08ba682768ecef439c407be8f0437ae
Marissa Zhong <mwz2101@columbia.edu>
2018-08-07 19:52:55 +0000

    automatically backed up by learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -2,5 +2,7 @@ export function managePresents(state={numberOfPresents: 0,}, action){
   switch (action.type) {
     case 'INCREASE':
       return {numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return {numberOfPresents: 0}
   }
 }
