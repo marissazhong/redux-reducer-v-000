954873f24cf45600a7b56ad622a82e336ecefad5
Marissa Zhong <mwz2101@columbia.edu>
2018-08-07 20:00:59 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,9 @@
 export function manageFriends(state, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      return { ...state, state.friends: [...state.friends, action.friend]}
+      return { ...state, friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      return { ...state, friends: [...state.friends, action.friend]}
     default:
       return { friends: [ { name: 'Avi', hometown: 'NYC', id: 100 } ]}
   }
