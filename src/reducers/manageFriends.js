c7e9faaa649836886713034222cf7751f18e40ab
Marissa Zhong <mwz2101@columbia.edu>
2018-08-07 20:02:44 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { ...state, friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      const i = state.friends.findIndex(item => item.id === action.id)
+      return { ...state, friends: [...state.friends.slice(0, i),...state.friends.slice(i+1)]}
+    default:
+      return state;
+  }
 }
